1ba4a4dfeb1087fc83aaf32fb8360f6d9143da32
stage7 <stage7@stg7.net>
2015-02-18 23:24:03 +0100

    Project cleaning, fixed step synchro

--- js/test.js
+++ js/test.js
@@ -26,7 +26,10 @@ var timer;
 //-----
 //SONGS
 //-----
-var errorMargin = 0.5; // maximum threshold in bpm by default/exceed to successfully play a step
+var errorMargin = 0.5; //maximum threshold in bpm by default/exceed to successfully play a step
+var stepsArray; //basic step data: number of steps, whether it is playable or not, playability range
+var stepsArrayKeys; //stores the measures on which every song step is triggered
+var currentStep; //step number (not measure) to be played
 load_songs();
 
 //TODO: Change to load proper song
@@ -35,14 +38,15 @@ function startGame(){
 	//setTimeout(function(){console.log(songs);},4000);
 	start = new Date();
 
-	//Initialize some song stuff: array of successful steps, step error margin, etc.
+	//Initialize some song stuff: object of successful steps, step error margin, etc.
 	//Format of stepsArray object: {step_number: [steps, is_playable, margin_by_default, margin_by_excess]}
 	//	float	step_number: equals measure defined in song.js
 	//	int		steps: number of steps needed to complete this step, equals length of second item in each song item
 	//	bool	is_playable: defines if this note can still be played or otherwise was missed, defaults to true
 	//	float	margin_by_default: the amount of measures a step can be played before the actual step happens
 	//	float	margin_by_excess: the amount of measures a step can be played after the actual step happens
-	var stepsArray = {};
+	stepsArray = {};
+	stepsArrayKeys = [];
 	for(var i=0; i<song.song.length; i++){
 		if(i==0){
 			var marginByDefault = song.song[i][0] - errorMargin;
@@ -54,8 +58,11 @@ function startGame(){
 		}else{
 			var marginByExcess = song.song[i][0] + Math.min(errorMargin, (song.song[i+1][0]-song.song[i][0])/2);
 		}
-		stepsArray[song.song[i][0]] = [song.song[i][1].length, true, marginByDefault, marginByExcess];
+		stepsArray[song.song[i][0]] = {steps:song.song[i][1].length, is_playable:true, margin_by_default:marginByDefault, margin_by_excess:marginByExcess};
+		stepsArrayKeys.push(song.song[i][0]);
 	}
+
+	currentStep = 0;
 
 	timer = new Tock({
 		interval: 10,
@@ -63,6 +70,7 @@ function startGame(){
 	});
 	
 	console.log(stepsArray);
+	//console.log(stepsArrayKeys);
 	timer.start();
 	//gameLoop(song);
 }
@@ -86,44 +94,42 @@ function gameLoop(song) {
 
 function testKeys() {
 	/* Keys down */
-	if (keyState[37] || keyState[65]){ // left
+	if (keyState[37] || keyState[65]){ // left, 'a'
 		console.log("left");
 		keyLeft = true;
 		context.drawImage(img[0],32,80,96,96);
 	}
-	if (keyState[38] || keyState[87]){ // up
+	if (keyState[38] || keyState[87]){ // up, 'w'
 		console.log("up");
 		keyUp = true;
 		context.drawImage(img[1],192,80,96,96);
 	}
-	if (keyState[39] || keyState[68]){ // right
+	if (keyState[39] || keyState[68]){ // right, 'd'
 		console.log("right");
 		keyRight = true;
 		context.drawImage(img[3],512,80,96,96);
 	}
-	if (keyState[40] || keyState[83]){ // down
+	if (keyState[40] || keyState[83]){ // down, 's'
 		console.log("down");
 		keyDown = true;
 		context.drawImage(img[2],352,80,96,96);
 	}
 
-
-
 	/* Keys up */
-	if (!keyState[37] && !keyState[65]){ // left
+	if (!keyState[37] && !keyState[65]){ // left, 'a'
 		//console.log("no left");
 		keyLeft = false;
 		context.drawImage(img[0],16,64);
 	}
-	if (!keyState[38] && !keyState[87]){ // up
+	if (!keyState[38] && !keyState[87]){ // up, 'w'
 		keyUp = false;
 		context.drawImage(img[1],176,64);
 	}
-	if (!keyState[39] && !keyState[68]){ // right
+	if (!keyState[39] && !keyState[68]){ // right, 'd'
 		keyRight = false;
 		context.drawImage(img[3],496,64);
 	}
-	if (!keyState[40] && !keyState[83]){ // down
+	if (!keyState[40] && !keyState[83]){ // down, 's'
 		keyDown = false;
 		context.drawImage(img[2],336,64);
 	} 
@@ -131,12 +137,25 @@ function testKeys() {
 
 function drawSong(song, time) {
 	var currentMeasure = Math.round((song.bpm/60)*(time/1000)*10000) / 10000;
-	//console.log(currentMeasure);
+	//console.log(currentStep);
+	//if(stepsArrayKeys.hasOwnProperty(currentStep))
+	//	console.log(stepsArray[stepsArrayKeys[currentStep]].margin_by_excess + " -- " + currentMeasure);
 
 	for(var i=0; i<song.song.length; i++){
 		for(var noteSteps=0; noteSteps<song.song[i][1].length; noteSteps++){
 			//console.log(song.song[i][1][noteSteps]);
-			context.drawImage(img[song.song[i][1][noteSteps]],16+song.song[i][1][noteSteps]*160,song.song[i][0]*(96+32*song.difficulty)-((96+32*song.difficulty)*currentMeasure));
+			var yPos = song.song[i][0]*(96+32*song.difficulty)-((96+32*song.difficulty)*currentMeasure)+64;
+			if(yPos > -128 && yPos < 1088)
+				context.drawImage(img[song.song[i][1][noteSteps]],16+song.song[i][1][noteSteps]*160,yPos);
 		}
+	}
+
+	//console.log(Math.floor(currentMeasure));
+	if(stepsArrayKeys.hasOwnProperty(currentStep)){
+		if(currentMeasure > stepsArray[stepsArrayKeys[currentStep]].margin_by_excess)
+			stepsArray[stepsArrayKeys[currentStep]].is_playable = false;
+
+		if(currentMeasure > stepsArray[stepsArrayKeys[currentStep]].margin_by_excess && !stepsArray[stepsArrayKeys[currentStep]].is_playable)
+			currentStep++;
 	}
 }
